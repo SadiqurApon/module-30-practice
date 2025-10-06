2fae2534aa0613fb7b03c5622a7b79327e62fca2
SadiqurApon <sadiqurrahmanapon34@gmail.com>
2025-10-06 21:26:14 +0600

    Recape Array Mathod

---
+++ recapArroyMathods.js
@@ -0,0 +1,15 @@
+const products = [
+    {id:1, name: "iphone", color: "black", price: 1200, brand: "apple"},
+    {id:2, name: "xiaomi", color: "gold", price: 100, brand: "xiaomi"},
+    {id:3, name: "samsung", color: "gold", price: 1500, brand: "sam"},
+    {id:4, name: "iphone", color: "gold", price: 1000, brand: "apple"},
+    {id:5, name: "xiaomi", color: "black", price: 1100, brand: "xiaomi"}
+];
+
+const newProducts = products.map(p => {
+    if(p.brand === "apple"){
+        p.price = p.price + 100;
+    }
+    return p;
+})
+console.log(newProducts);
