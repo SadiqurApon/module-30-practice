8471a74c537a99d296fae8d611420a9fa0a69a1f
SadiqurApon <sadiqurrahmanapon34@gmail.com>
2025-10-06 21:01:39 +0600

    Array Mathods practice

---
+++ arrayMathods.js
@@ -0,0 +1,20 @@
+const products = [
+    {id:1, name: "iphone", color: "black", price: 1200, brand: "apple"},
+    {id:2, name: "xiaomi", color: "gold", price: 100, brand: "xiaomi"},
+    {id:3, name: "samsung", color: "gold", price: 1500, brand: "sam"},
+    {id:4, name: "iphone", color: "gold", price: 1000, brand: "apple"},
+    {id:5, name: "xiaomi", color: "black", price: 1100, brand: "xiaomi"}
+];
+
+const product = products.find(p => p.id ===5);
+console.log(product);
+
+// const newProducts = products.filter(product => product.price > 1000);
+// console.log(newProducts);
+
+
+// products.forEach(product => {
+//     if(product.color === "gold"){
+//         console.log(product);
+//     }
+// })
